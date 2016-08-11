1341a8e69f176fff77398a40215d0c213307f2fa
Jeremy Sklarsky <jsklarsky@mediamath.com>
2016-08-11 19:31:58 -0400

    format dates and authors on 538 feed

--- src/components/election-dash-article/election-dash-article.js
+++ src/components/election-dash-article/election-dash-article.js
@@ -13,6 +13,13 @@
       }
     },
 
+    ready: function() {
+      this.formattedDate = moment(this.model.date).format('MMMM Do, h:mm a');
+      this.formattedAuthor = this.model.author.length > 22 ?
+        this.model.author.substring(0,19) + '...' :
+        this.model.author.substring(0,19);
+    },
+
     _click: function() {
       var drawer = this.querySelector('#drawer_' + this.model.index);
 
